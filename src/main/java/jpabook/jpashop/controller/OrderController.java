709f24aaba4aaa017cbb1d06b4806e2c91a2a63a
l6778 <l6778@naver.com>
2023-07-06 00:59:51 +0900

    feat, refactor: orderController 및 기능 추가, orderService,Test 코드 수정

--- src/main/java/jpabook/jpashop/controller/OrderController.java
+++ src/main/java/jpabook/jpashop/controller/OrderController.java
@@ -1,11 +1,62 @@
 package jpabook.jpashop.controller;
 
+import java.util.List;
+import jpabook.jpashop.Service.ItemService;
+import jpabook.jpashop.Service.MemberService;
+import jpabook.jpashop.Service.OrderService;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.OrderSearch;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
 public class OrderController {
-//    private final OrderService
+    private final OrderService orderService;
+    private final MemberService memberService;
+    private final ItemService itemService;
+
+    @GetMapping("/order")
+    public String createForm(Model model) {
+        List<Member> members = memberService.findMembers();
+        List<Item> items = itemService.findItems();
+
+        model.addAttribute("members", members);
+        model.addAttribute("items", items);
+
+        return "order/orderForm";
+    }
+
+    @PostMapping("/order")
+    public String order(@RequestParam("memberId") Long memberId,
+                        @RequestParam("itemId") Long itemId,
+                        @RequestParam("count") int count) {
+        orderService.order(memberId, itemId, count);
+        return "redirect:/orders";
+    }
+
+    @GetMapping("/orders")
+    public String orderList(@ModelAttribute("orderSearch")OrderSearch orderSearch, Model model) {
+        List<Order> orders = orderService.findOrders(orderSearch);//단순 위임이면 그냥 Controller에서 해도 상관은 없다.
+        model.addAttribute("orders", orders);
+
+        return "order/orderList";
+    }
+
+    @PostMapping("/orders/{orderId}/cancel")
+    public String cancelOrder(@PathVariable("orderId") Long orderId) {
+        orderService.cancelOrder(orderId);
+
+        return "redirect:/orders";
+    }
 
 }
--- src/test/java/jpabook/jpashop/Service/OrderServiceTest.java
+++ src/test/java/jpabook/jpashop/Service/OrderServiceTest.java
@@ -40,11 +40,19 @@ public class OrderServiceTest {
     public void 상품주문() throws Exception {
         //given
         Member member = createMember();
+        member.setName("회원1");
+        member.setAddress(new Address("서울","강가","123-123"));
 
-        Book book = createBook("시골 JPA", 10000, 10);
+        em.persist(member);
+
+        Book book = new Book();
+        book.setName("시골 JPA");
+        book.setPrice(10000);
+        book.setStockQuantity(10);
+        em.persist(book);
+
 
         int orderCount = 2;
-
         //when
         Long orderId = orderService.order(member.getId(), book.getId(), orderCount);
 
@@ -52,9 +60,9 @@ public class OrderServiceTest {
         Order getOrder = orderRepository.findOne(orderId);
 
         assertEquals("상품 주문시 상태는 ORDER", OrderStatus.ORDER, getOrder.getStatus());
-        assertEquals("주문한 상품 종류 수가 정확", 1, getOrder.getOrderItems().size());
-        assertEquals("주문 가격은 가격 * 수량.", 10000 * orderCount, getOrder.getTotalPrice());
-        assertEquals("주문 수량만큼 재고가 줄어야 함", 8, book.getStockQuantity());
+        assertEquals("주문한 상품 종류 수가 정확해야 한다.", 1, getOrder.getOrderItems());
+        assertEquals("주문 가격은 가격 * 수량이다.", 10000*orderCount, getOrder.getTotalPrice());
+        assertEquals("주문 수량만큼 재고가 줄어야 한다.", 8, book.getStockQuantity());
     }
 
     @Test
